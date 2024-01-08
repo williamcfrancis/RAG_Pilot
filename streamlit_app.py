57a5f225cdd61ff1081de4e95ede2ac5d32075e6
William C Francis <williamcfranco@gmail.com>
2024-01-08 17:24:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from llama_index import SimpleDirectoryReader
 st.set_page_config(page_title="Personalized Chat with Philly AI Connect", page_icon="💬", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key 
 st.title("Personalized Chat with Philly AI Connect")
-st.info("This is a pilot project to test out Retrieval Augmented Generation (RAG) with LLamaIndex and GPT 3.5", icon="📃")
+st.info("This is a pilot project to test out Retrieval Augmented Generation (RAG) for personalized client interactions, created using LLamaIndex and GPT 3.5", icon="📃")
          
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
